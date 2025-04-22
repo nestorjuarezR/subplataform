0e0b88c19b5954c63570244aa74766a4a92f0596
nestorjuarezr <nesk.company@gmail.com>
2025-04-21 20:20:38 -0600

    Create a CustomUser model as an AbstractBaseUser

--- account/models.py
+++ account/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from managers import CustomUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+    
+
+
+
+
