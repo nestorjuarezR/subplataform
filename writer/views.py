ef5e995a9545e2529d7997f4a8788d11246d09e8
nestorjuarezr <nesk.company@gmail.com>
2025-05-15 14:32:52 -0600

    Create a basic view to see articles for writer

--- writer/views.py
+++ writer/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ArticleForm
 from django.http import HttpResponse
+from .models import Article
+
+
 @login_required(login_url='login')
 def writer_dashboard(request):
     return render(request, 'writer/writer-dashboard.html')
@@ -20,7 +23,16 @@ def create_article(request):
             article.user = request.user
             article.save()
 
-            return HttpResponse('Article created successfully!')
+            return redirect('my_articles')
         
     context = {'form': form}
     return render(request, 'writer/create-article.html', context=context)
+
+
+@login_required(login_url='login')
+def my_articles(request):
+    current_user = request.user
+    articles = Article.objects.filter(user=current_user)
+
+    context = {'articles': articles}
+    return render(request, 'writer/my-articles.html', context=context)
