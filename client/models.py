b4ca242157b4c0dc4f84d507ad88f49c9156a869
nestorjuarezr <nesk.company@gmail.com>
2025-06-07 09:30:33 -0600

    Create subscription model on client app

--- client/models.py
+++ client/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from account.models import CustomUser
 
-# Create your models here.
+
+
+class Subscription(models.Model):
+    subscriber_name = models.CharField(max_length=300)
+    subscription_plan = models.CharField(max_length=255)
+    subscription_cost = models.CharField(max_length=255)
+    paypal_susbscription = models.CharField(max_length=300)
+    is_activate = models.BooleanField(default=False)
+    user = models.OneToOneField(CustomUser, max_length=10, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.subscriber_name} - {self.subscription_plan} - {self.subscription_cost} - {self.paypal_susbscription} - {self.is_activate}"
